ace0c81e8e81ab05acd0a573f9e8368f27442cf4
Felipe Brito <felipeb@clubefii.com.br>
2022-09-14 12:52:49 -0300

    first commit

---
+++ CloudCustomers.UnitTests/Fixtures/UsersFixture.cs
@@ -0,0 +1,49 @@
+﻿using CloudCustomers.API.Models;
+using System.IO;
+using System.Reflection.Emit;
+
+namespace CloudCustomers.UnitTests.Fixtures
+{
+    public static class UsersFixture
+    {
+        public static List<User> GetTestUsers() => new()
+        {
+            new User
+            {
+                Id = 1,
+                Name = "Felipe",
+                Address = new Address()
+                {
+                    Street = "Rua 1",
+                    City = "Ouro Preto",
+                    ZipCode = "12345"
+                },
+                Email = "felipe@example.com"
+            },
+            new User
+            {
+                Id = 2,
+                Name = "Maycon",
+                Address = new Address()
+                {
+                    Street = "Rua 1",
+                    City = "Ipatinga",
+                    ZipCode = "22345"
+                },
+                Email = "maycon@example.com"
+            },
+            new User
+            {
+                Id = 3,
+                    Name = "Valeria",
+                    Address = new Address()
+                    {
+                        Street = "Rua 3",
+                        City = "Monlevade",
+                        ZipCode = "32345"
+                    },
+                    Email = "valeria@example.com"
+            }
+        };
+    }
+}
---
+++ CloudCustomers.UnitTests/Systems/Services/TestUsersService.cs
@@ -0,0 +1,104 @@
+﻿using CloudCustomers.API.Models;
+using CloudCustomers.API.Models.Config;
+using CloudCustomers.API.Services;
+using CloudCustomers.UnitTests.Fixtures;
+using CloudCustomers.UnitTests.Helpers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Moq;
+using Moq.Protected;
+using System.Net;
+
+namespace CloudCustomers.UnitTests.Systems.Services
+{
+    public class TestUsersService
+    {
+        [Fact]
+        public async Task GetAllUsers_WhenCalled_InvokesHttpGetRequest()
+        {
+            //Arrange
+            var expectedResponse = UsersFixture.GetTestUsers();
+            var handlerMock = MockHttpMessageHandler<User>.SetupBasicGetResourceList(expectedResponse);
+            var httpClient = new HttpClient(handlerMock.Object);
+            var endpoint = "https://example.com";
+            var config = Options.Create(new UsersApiOptions
+            {
+                Endpoint = endpoint
+            });
+            var sut = new UsersService(httpClient, config);
+
+            //Act
+            await sut.GetAllUsers();
+
+            //Assert
+            handlerMock
+                .Protected()
+                .Verify(
+                "SendAsync",
+                Times.Exactly(1),
+                ItExpr.Is<HttpRequestMessage>(req => req.Method == HttpMethod.Get),
+                ItExpr.IsAny<CancellationToken>()
+            );
+        }
+
+        [Fact]
+        public async Task GetAllUsers_WhenCalled_ReturnsListOfUsers()
+        {
+            //Arrange
+            var expectedResponse = UsersFixture.GetTestUsers();
+            var handlerMock = MockHttpMessageHandler<User>.SetupBasicGetResourceList(expectedResponse);
+            var httpClient = new HttpClient(handlerMock.Object);
+            var endpoint = "https://example.com";
+            var config = Options.Create(new UsersApiOptions
+            {
+                Endpoint = endpoint
+            });
+            var sut = new UsersService(httpClient, config);
+
+            //Act
+            var result = await sut.GetAllUsers();
+
+            //Assert
+            result.Count.Should().Be(expectedResponse.Count);
+        }
+
+        [Fact]
+        public async Task GetAllUsers_WhenCalled_InvokesConfiguredExternalUrl()
+        {
+            //Arrange
+            var expectedResponse = UsersFixture.GetTestUsers();
+            var endpoint = "https://example.com/users";
+            var handlerMock = MockHttpMessageHandler<User>
+                .SetupBasicGetResourceList(expectedResponse, endpoint);
+
+            var httpClient = new HttpClient(handlerMock.Object);
+
+            var config = Options.Create(
+                new UsersApiOptions
+                {
+                    Endpoint = endpoint
+                }
+            );
+
+            var sut = new UsersService(httpClient, config);
+
+            //Act
+            var result = await sut.GetAllUsers();
+
+            var uri = new Uri(endpoint);
+
+            //Assert
+            handlerMock
+                .Protected()
+                .Verify(
+                "SendAsync",
+                Times.Exactly(1),
+                ItExpr.Is<HttpRequestMessage>(
+                    req => req.Method == HttpMethod.Get 
+                    && req.RequestUri == uri),
+                ItExpr.IsAny<CancellationToken>()
+            );
+        }
+    }
+}
